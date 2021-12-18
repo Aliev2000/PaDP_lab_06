1feb598acf49f5bb31bcfe1abc783c6ee4c069b0
Parviz <alievz2011@gmail.com>
2021-12-18 07:09:23 +0300

    Scripted auto-commit on change (2021-12-18 07:09:23) by gitwatch.sh

--- src/main/java/lab6/ConfigStorageActor.java
+++ src/main/java/lab6/ConfigStorageActor.java
@@ -6,6 +6,7 @@ import akka.japi.pf.ReceiveBuilder;
 import java.util.*;
 
 public class ConfigStorageActor extends AbstractActor {
+    @Override
     private void saveServerList(ServerList list) {
         data = new ArrayList<>(list.getData());
         System.out.println(data);
